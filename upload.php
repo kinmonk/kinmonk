ac5f35d1a3cef2d0991564a8ec428bc86eb448aa
kinmonk <98945254+kinmonk@users.noreply.github.com>
2022-02-11 15:24:45 +0100

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php
+$filename = $_FILES['file']['name'];
+$location = "upload/".$filename;
+if( move_uploaded_file($_FILES['file']['tmp_name'], $location)){
+echo '<p>File uploaded successfully</p>';
+}else{
+echo '<b>Error uploading file</b>';
+}
+?>
+</body>
+</html>
---
+++ uploader.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> PHP File Upload Example </title>
+  </head>
+
+  <body>
+
+  <form action="upload.php" method="post" enctype="multipart/form-data" >
+    <input type="file" name="file" />
+    <input type="submit" value="Upload" />
+  </form>
+
+  </body>
+</html>
